1d60d3a04386d36a57f74908339dd46317a024d6
jola <jola.j.tian@hotmail.com>
2021-07-24 14:09:41 +1000

    updated function.py and invoice-mapping.py

--- function.py
+++ function.py
@@ -1,5 +1,9 @@
-def my_first_function():
-  print("Hello, this is my first function.")
+def print_hello_world():
+	print("Hello world.")
+
+def print_10_times_hello_world():
+  for i in range(0,11):
+	  print_hello_world()
 
 def function_with_param(variable):
 	print("Hello, this is my variable: " + variable)
@@ -8,19 +12,79 @@ def function_with_multiple_params(variable1, variable2):
 	print("Hello, this is my first variable: " + variable1)
 	print("Hello, this is my second variable: " + variable2)
 
-def print_name_and_age(name, gender):
-	print("My name is" + name+",  and my gender is "+gender+".")
+def print_name_and_gender(name, gender):
+	print("My name is" + name +",  and my gender is "+gender+".")
 
-## Running Program
-my_first_function()
 
-variable = "a"
-function_with_param(variable)
+# take 2 params e.g. a and b
+# return sum of a and b
+def sum(part1, part2):
+	return part1 + part2
 
-variable1 = "a"
-variable2 = "b"
-function_with_multiple_params(variable1, variable2)
+# ## Running Program
+# my_first_function()
 
-name = "Jola"
-gender = "female"
-print_name_and_age(name, gender)
+# variable = "a"
+# function_with_param(variable)
+
+# variable1 = "a"
+# variable2 = "b"
+# function_with_multiple_params(variable1, variable2)
+
+# name = "Jola"
+# gender = "female"
+# print_name_and_gender(name, gender)
+
+# print_10_times_hello_world()
+
+# part1 = 1
+# part2 = 2
+# result = sum(part1,part2)
+# print(result)
+
+# 写一个函数，计算参数1与参数2的差值（相减）
+# 并且判断结果是不是大于10
+# 如果是，返回true; 否，返回false
+
+# 变量的作用域
+# file 作用域： 全局作用域
+# 函数作用域： 局部作用域
+def minus(part1, part2):
+	result = part1 - part2
+	if result > 10:
+		return True
+	else:
+		return False
+
+part1 = 30
+part2 = 14
+result = minus(part1, part2) # True
+result = False
+
+
+# 定义一个函数，计算参数1与参数2的积（相乘）
+# 定义另外一个函数，如下：
+# 计算 6和7的积
+# 计算 3和8的积
+# 计算两个积之间的差值，如果差值小于10，则打印“积小于10”；如果大于10，则打印“大于10”10次
+
+def multiply(part1, part2):
+	return part1 * part2
+
+	
+part1 = 6
+part2 = 7
+result1 = multiply(part1, part2)
+part1 = 4
+part2 = 8
+result2 = multiply(part1, part2)
+
+def minus(result1, result2):
+	minus = result1 - result2
+	if minus < 10:
+		print("minus < 10")
+	else:
+		for i in range(0,11):
+			print("minus > 10")
+
+minus(result1, result2)
--- invoice-mapping.py
+++ invoice-mapping.py
@@ -5,13 +5,42 @@ invoice_records = [
 {"company": "JB HIFI","category":"","department":""},
 {"company": "Optus Store","category":"","department":""}
 ]
- 
+
+
 mapping = {
 "Harvay Normen": {"category":"Sale","department":"Marketing"},
 "5G Works": {"category":"Rebate","department":"Operation"},
 "JB HIFI": {"category":"Discount","department":"Account"},
 "Optus Store": {"category":"Annal Bouns","department":"IT"},   
 }
+
+for invoice in invoice_records:
+    # invoice = {"company": "Harvay Normen","category":"","department":""},
+    
+    target_company = invoice["company"]
+    # >> target_company = "Harvay Normen"
+
+    mapping_company = mapping[target_company] 
+    # >> mapping_company = {"category":"Sale","department":"Marketing"}
+
+    invoice["category"] = mapping_company["category"] 
+    # >> mapping_company["category"] = "Sale"
+    # >> invoice["category"] = "Sale"
+
+    invoice["department"] = mapping_company["department"] 
+    # >> mapping_company["department"] = "Marketing"
+    # >> invoice["department"] = "Marketing"
+
+    # loop 4 times until reach the end of mapping list
+    
+    # before:
+    # invoice = {"company": "Harvay Normen","category":"","department":""},
+
+    # After
+    # >> invoice["category"] = "Sale"
+    # >> invoice["department"] = "Marketing"
+    # invoice = {"company": "Harvay Normen","category":"Sale","department":"Marketing"},
+
 
 
 print("")
@@ -29,10 +58,10 @@ print("After:")
 for invoice in invoice_records:
     target_company = invoice["company"]
     mapping_company = mapping[target_company] 
-    invoice["category"] = mapping_company["category"]
-    invoice["department"] = mapping_company["department"]
+    invoice["category"] = mapping_company["category"] 
+    invoice["department"] = mapping_company["department"] 
     
-# After
+
 for invoice in invoice_records:
     print(invoice)
 print("")
